2c1237684cdc6c190c17e429c110536f10fa0649
Oscar0871200 <oscarfredes0087@gmail.com>
2024-09-30 07:52:29 -0300

    Add files via upload

---
+++ restinicial-main/src/main/java/com/example/inicial1/entities/Domicilio.java
@@ -0,0 +1,23 @@
+package com.example.inicial1.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@ToString
+@Builder
+public class Domicilio {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="calle")
+    private String calle;
+
+    @Column(name="numero")
+    private int numero;
+}
